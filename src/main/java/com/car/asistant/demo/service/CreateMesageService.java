03bcebba026c1c079c357ee68b6a6f0b7c2569b1
Kuba <kuba@email>
2019-12-30 21:22:00 +0100

    Add MesageService with implementation

---
+++ src/main/java/com/car/asistant/demo/service/CreateMesageService.java
@@ -0,0 +1,11 @@
+package com.car.asistant.demo.service;
+
+import com.car.asistant.demo.dto.CarUserToInformationDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CreateMesageService {
+
+String createInformationMessage(CarUserToInformationDto carUserToInformationDto);
+
+}
---
+++ src/main/java/com/car/asistant/demo/service/impl/CreateMesageImpl.java
@@ -0,0 +1,34 @@
+package com.car.asistant.demo.service.impl;
+
+import com.car.asistant.demo.dto.CarUserToInformationDto;
+import com.car.asistant.demo.service.CreateMesageService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CreateMesageImpl implements CreateMesageService {
+    @Override
+    public String createInformationMessage(CarUserToInformationDto carUserToInformationDto) {
+
+        String message;
+
+        message = "<h1>Witaj " + carUserToInformationDto.getFirstName() + "<h1><br><br><br>"
+                + "Twój " + carUserToInformationDto.getCarModel() + " wymaga: ";
+        if (carUserToInformationDto.getOil() == 1) {
+            message = message + "wymiany oleju ";
+        }
+        if (carUserToInformationDto.getTimingGear() == 1) {
+            message = message + "przeglądu skrzyni biegów ";
+        }
+        if (carUserToInformationDto.getBreakPads() == 1) {
+            message = message + " wymiany klocków hamulcowych";
+        }
+
+        message = message + "<br><br><br> <h3>Pozdrawia zespół Car Assistant<h3>";
+
+        return message;
+
+    }
+
+
+}
